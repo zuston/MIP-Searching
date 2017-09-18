0435063300eb6ed8f292f68f44a4f108c515962b
lab-zuston <zuston.shacha@gmail.com>
2017-09-18 21:19:47 +0800

    engine continue

--- src/main/java/io/github/zuston/WorkFlow/WfBean/WorkFlowBean.java
+++ src/main/java/io/github/zuston/WorkFlow/WfBean/WorkFlowBean.java
@@ -7,4 +7,15 @@ public class WorkFlowBean {
     public Integer sourceID;
     public Integer nextTrueTargetID;
     public Integer nextFalseTargetID;
+    public Integer type;
+    public WorkFlowBean nextTrue;
+    public WorkFlowBean nextFalse;
+
+    public Integer getSourceID() {
+        return sourceID;
+    }
+
+    public void setSourceID(Integer sourceID) {
+        this.sourceID = sourceID;
+    }
 }
--- src/main/java/io/github/zuston/WorkFlow/WorkFlowEngine.java
+++ src/main/java/io/github/zuston/WorkFlow/WorkFlowEngine.java
@@ -9,9 +9,7 @@ import io.github.zuston.WorkFlow.WfBean.WorkFlowBean;
 import io.github.zuston.WorkFlow.WfSupport.EdgeConst;
 
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
@@ -38,7 +36,7 @@ public class WorkFlowEngine {
         return false;
     }
 
-    private void generateWf(String workFlowJson) {
+    private void generateWf(String workFlowJson) throws Exception {
         parse(workFlowJson);
 
         // 获取以 id　为　key　的值的节点
@@ -48,12 +46,18 @@ public class WorkFlowEngine {
         questionHm =
                 questionNodes.stream().collect(Collectors.toMap(QuestionNode::getId,c->c));
 
-        // 将　json　解析为链表
+        // 将　json　解析为 list
         ArrayList<WorkFlowBean> beansLinkedList = new ArrayList<>();
         for (EdgeNode edgeNode : edgeNodes){
 
             if (edgeNode.type == EdgeConst.QUESTION_ACTION){
                 WorkFlowBean existBean = haveExistedBean(beansLinkedList,edgeNode.sourceID);
+                if (existBean == null){
+                    existBean = new WorkFlowBean();
+                    beansLinkedList.add(existBean);
+                }
+                existBean.sourceID = edgeNode.sourceID;
+                existBean.type = edgeNode.type;
                 if (edgeNode.conditionRes){
                     existBean.nextTrueTargetID = edgeNode.targetID;
                 }else {
@@ -65,21 +69,83 @@ public class WorkFlowEngine {
             WorkFlowBean workFlowBean = new WorkFlowBean();
             workFlowBean.sourceID = edgeNode.sourceID;
             workFlowBean.nextTrueTargetID = edgeNode.targetID;
+            workFlowBean.type = edgeNode.type;
             beansLinkedList.add(workFlowBean);
         }
-        core(beansLinkedList);
+        //　流程变为
+        WorkFlowBean startNode = core(beansLinkedList);
     }
 
-    //　流模型解析，映射到　script　生成
-    private void core(ArrayList<WorkFlowBean> beansLinkedList) {
+    //　arrayList ---> linkedList
+    private WorkFlowBean core(ArrayList<WorkFlowBean> beanArrayList) throws Exception {
+        //　show
+//        for (WorkFlowBean workFlowBean : beanArrayList){
+//            System.out.println(workFlowBean.sourceID + " : "+workFlowBean.nextTrueTargetID + " : "+workFlowBean.nextFalseTargetID +" :" +workFlowBean.type);
+//        }
 
+        Map<Integer,WorkFlowBean> arrayHm = beanArrayList.stream().collect(Collectors.toMap(
+                WorkFlowBean::getSourceID,c->c
+        ));
+        // 遍历节点
+        // 获取初始节点，只有出没有进,即只有sourceId 未成为别人的 targetID
+        Set<Integer> existId = new HashSet<>();
+        for (WorkFlowBean flowBean : beanArrayList){
+            existId.add(flowBean.sourceID);
+            if (flowBean.nextFalseTargetID != null){
+                existId.add(flowBean.nextFalseTargetID);
+            }
+            if (flowBean.nextTrueTargetID != null){
+                existId.add(flowBean.nextTrueTargetID);
+            }
+        }
+
+        for (WorkFlowBean flowBean : beanArrayList){
+            flowBean.nextTrue = arrayHm.get(flowBean.nextTrueTargetID);
+            if (flowBean.type == EdgeConst.QUESTION_ACTION){
+                flowBean.nextFalse = arrayHm.get(flowBean.nextFalseTargetID);
+            }
+
+            // 获取初始节点
+            if (flowBean.nextFalseTargetID != null && existId.contains(flowBean.nextFalseTargetID))
+                existId.remove(flowBean.nextFalseTargetID);
+            if (flowBean.nextTrueTargetID != null && existId.contains(flowBean.nextTrueTargetID))
+                existId.remove(flowBean.nextTrueTargetID);
+        }
+
+        if (existId == null || existId.size() != 1)
+            throw new Exception("json error");
+
+        // 获取初始节点
+        WorkFlowBean startNode = arrayHm.get(existId.toArray()[0]);
+
+        WorkFlowBean Node = arrayHm.get(existId.toArray()[0]);
+
+        while (startNode!=null){
+            if (startNode.nextTrue == null){
+                WorkFlowBean endNode = new WorkFlowBean();
+                endNode.sourceID = startNode.nextTrueTargetID;
+                endNode.type = 0;
+                startNode.nextTrue = endNode;
+                break;
+            }else
+                startNode = startNode.nextTrue;
+        }
+
+        WorkFlowBean sb = new WorkFlowBean();
+        sb = Node;
+        while (Node!=null){
+            System.out.println(String.format("sourceId:%s,targetId:%s,type:%s",Node.sourceID,Node.nextTrueTargetID,Node.type));
+            Node = Node.nextTrue;
+        }
+
+        return sb;
     }
 
     private WorkFlowBean haveExistedBean(ArrayList<WorkFlowBean> beansLinkedList, int sourceID) {
         for (WorkFlowBean bean : beansLinkedList){
-            if (bean.sourceID == sourceID)  return bean;
+            if (bean.sourceID != null && bean.sourceID == sourceID)  return bean;
         }
-        return new WorkFlowBean();
+        return null;
     }
 
 //    private void core(int[] flowContainer, int start) {
